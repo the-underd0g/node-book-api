3c03e3de754b58cedb3f4d4deea6fdd2e5cde2f1
underd0g <underd0g.3003@gmail.com>
2019-05-18 22:58:43 -0300

    refactoring the app.js file

--- app.js
+++ app.js
@@ -5,41 +5,10 @@ const db = mongoose.connect('mongodb://localhost:37017/bookAPI',{ useNewUrlParse
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
 app.use(bodyParser.urlencoded({extended :true}));
 app.use(bodyParser.json());
-
-const bookRouter = express.Router();
-
-bookRouter.route('/books')
-    .post(
-        (req,res ) => {
-            const book = new Book(req.body);
-            book.save();
-            return res.status(201).json(book);
-        }
-    )
-    .get((req, res) => {
-        const query = {};
-        if ( req.query.genre) {
-            query.genre = req.query.genre
-        }
-        Book.find(query, (err, books) => {
-            if(err){
-                return res.send(err)
-            }
-            return res.json(books);
-
-        })
-    });
-bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
-            if(err){
-                return res.send(err)
-            }
-            return res.json(book);
-        })
-    });
 
 app.use('/api', bookRouter);
 
