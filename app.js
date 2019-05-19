681ad048dfbc593a799243ef5b13ca751446b6eb
underd0g <underd0g.3003@gmail.com>
2019-05-18 22:34:27 -0300

    adding a filter and a get a book by bookId

--- app.js
+++ app.js
@@ -10,12 +10,25 @@ const bookRouter = express.Router();
 
 bookRouter.route('/books')
     .get((req, res) => {
-        Book.find((err, books) => {
+        const query = {};
+        if ( req.query.genre) {
+            query.genre = req.query.genre
+        }
+        Book.find(query, (err, books) => {
             if(err){
                 return res.send(err)
             }
             return res.json(books);
 
+        })
+    });
+bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if(err){
+                return res.send(err)
+            }
+            return res.json(book);
         })
     });
 
