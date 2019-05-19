64597e4e9a613e5229204b9b714dcc74b7f016ab
underd0g <underd0g.3003@gmail.com>
2019-05-18 23:24:08 -0300

    adding an small middleware function and patch endpoint

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -24,27 +24,51 @@ function routes(Book){
 
             })
         });
+    bookRouter.use('/books/:bookId', (req, res, next) =>{
+        Book.findById(req.params.bookId, (err, book) => {
+            if(err){
+                return res.send(err)
+            }
+            if(book){
+                req.book = book;
+                return next();
+            }else{
+                return res.sendStatus(404);
+            }
+        });
+    });
     bookRouter.route('/books/:bookId')
-        .get((req, res) => {
-            Book.findById(req.params.bookId, (err, book) => {
-                if(err){
-                    return res.send(err)
-                }
-                return res.json(book);
-            })
-        })
+        .get((req, res) => res.json(req.book))
         .put((req, res ) =>{
-            Book.findById(req.params.bookId, (err, book) => {
-                if(err){
-                    return res.send(err)
+            const { book } = req;
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.read;
+            req.book.save((err) => {
+                if (err) {
+                    return res.send(err);
                 }
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.genre = req.body.genre;
-                book.read = req.body.read;
-                book.save();
                 return res.json(book);
-            })
+            });
+        })
+        .patch((req, res) => {
+            const { book } = req;
+
+            if(req.body._id){
+                delete req.body._id;
+            }
+            Object.entries(req.body).forEach( item => {
+                const key = item[0];
+                const value = item[1];
+                book[key] = value;
+            });
+            req.book.save((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.json(book);
+            });
         });
     return bookRouter;
 }
