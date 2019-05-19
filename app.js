91aa398502eb31d94c8bc578513dd7d816de1caf
underd0g <underd0g.3003@gmail.com>
2019-05-18 22:30:06 -0300

    adding first books endpoint implementation

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-let express = require('express');
-
-let app = express();
-
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+const db = mongoose.connect('mongodb://localhost:37017/bookAPI',{ useNewUrlParser: true } );
 const port = process.env.PORT || 3000;
+
+const Book = require('./models/bookModel');
+
+const bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get((req, res) => {
+        Book.find((err, books) => {
+            if(err){
+                return res.send(err)
+            }
+            return res.json(books);
+
+        })
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my API');
